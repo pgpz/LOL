a506d4f583599256284582f47e4cc4f2c77994ae
pgp <admin@pgpkey.net>
2025-12-17 21:43:28 -0600

    ry4c_window
    displays ry4c text

---
+++ fuckthisshit.c
@@ -0,0 +1,25 @@
+#include <windows.h>
+
+int main()
+{
+  const char* title = "Ry4c owes me money!";
+  HWND hwnd = CreateWindowEx(
+    0, "RY4C", title,
+    WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+    100, 100, 300, 200,
+    NULL, NULL, NULL, NULL
+  );
+  if (!hwnd) return 1;
+  for (int i = 0; i < 20; i++)
+  {
+    ShowWindow(hwnd, SW_HIDE);
+    MessageBeep(MB_ICONASTERISK);
+    Sleep(2);
+    ShowWindow(hwnd, SW_SHOW);
+    MessageBeep(BM<ICONASTERISK);
+    Sleep(2);
+  }
+  DestroyWindow(hwnd);
+  return 0;
+}
+
